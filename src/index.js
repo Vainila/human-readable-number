9396dd0b18d2dada9b047544d54beaad8d2a5ab9
Vainila <workmail1421@gmail.com>
2021-02-21 14:06:58 +0300

    toReadable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,65 @@
-module.exports = function toReadable (number) {
-  
+let numVocabulary ={
+    1: "one ",
+    2: "two ",
+    3: "three ",
+    4: "four ",
+    5: "five ",
+    6: "six ",
+    7: "seven ", 
+    8: "eight ",
+    9: "nine ",
+    10: "ten ",
+    11: "eleven ",
+    12: "twelve ",
+    13: "thirteen ",
+    14: "fourteen ",
+    15: "fifteen ",
+    16: "sixteen ",
+    17: "seventeen ",
+    18: "eighteen ",
+    19: "nineteen ",
+    20: "twenty ",
+    30: "thirty ",
+    40: "forty ",
+    50: "fifty ",
+    60: "sixty ",
+    70: "seventy ",
+    80: "eighty ",
+    90: "ninety ",
+    100: "hundred ",
+    1000: "thousand "
+}
+
+module.exports = function toReadable (x) {    
+
+    if(x!=0) {numVocabulary[0]="";}
+    else{return "zero"}
+    let temp;
+    let res = "";
+    if(x>=100){
+        res+= numVocabulary[Math.floor(x/100)];
+        res+= numVocabulary[(100)];
+    }
+    
+    if(x>=10){
+        if(x>=100){temp = x%100;}else{temp = x;}        
+        if(temp>=20){
+            if(temp%10==0){
+                res+=numVocabulary[temp];
+            }
+            else{
+                res+=numVocabulary[temp-temp%10];
+                res+=numVocabulary[temp%10];
+            }
+        }else if(temp>=10 && temp<20){
+            res+=numVocabulary[temp];
+        }else{
+            res+=numVocabulary[temp];
+        }
+    }
+    else{
+        res+=numVocabulary[x];
+    }
+
+    return res.trim();  
 }
